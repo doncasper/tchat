962b5f18ae9218a2ec877fcd801f83e6c3572e33
Don Casper <doncasperlive@gmail.com>
2025-06-28 12:28:11 +0300

    Added gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tchat/',
   css: {
     modules: {
       localsConvention: 'camelCaseOnly', // Converts .chat-header to styles.chatHeader
